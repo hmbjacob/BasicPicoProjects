973df74dc2ce00d9856582fdd8136019e760bf76
Jacob McClellan <43324037+hmbjacob@users.noreply.github.com>
2024-07-20 13:51:51 -0700

    Led blinking on pico

--- baremetal/led-blink/main.c
+++ baremetal/led-blink/main.c
@@ -1,16 +1,12 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-#include <stdio.h>
-//#include "pico/stdlib.h"
+#include "pico/stdlib.h"
 
 int main() {
-    //stdio_init_all();
-    //while (true) {
-        //sleep_ms(1000);
-    //}
-    return 0;
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    while (true) {
+        gpio_put(PICO_DEFAULT_LED_PIN, 1);
+        sleep_ms(1000);
+        gpio_put(PICO_DEFAULT_LED_PIN, 0);
+        sleep_ms(1000);
+    }
 }
