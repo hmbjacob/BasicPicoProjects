d9a819fc0dbe9067eae47a31a7a26cc1897e3ecd
Jacob McClellan <43324037+hmbjacob@users.noreply.github.com>
2024-07-20 18:55:33 -0700

    working debugger

--- baremetal/led-blink/main.c
+++ baremetal/led-blink/main.c
@@ -1,12 +1,16 @@
 #include "pico/stdlib.h"
 
 int main() {
+    timer_hw->dbgpause = 0;
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    stdio_init_all();
     while (true) {
         gpio_put(PICO_DEFAULT_LED_PIN, 1);
-        sleep_ms(1000);
+        printf("Toggle on\n");
+        busy_wait_ms(1000);
         gpio_put(PICO_DEFAULT_LED_PIN, 0);
-        sleep_ms(1000);
+        printf("Toggle off\n");
+        busy_wait_ms(1000);
     }
 }
